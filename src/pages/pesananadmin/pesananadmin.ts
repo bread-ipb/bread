295799899450687a3f75ec0eeefbed5a6f831458
riyellevo <vauxhallvectravxr@gmail.com>
2017-12-14 03:25:54 +0700

    work pesanan

--- src/pages/pesananadmin/pesananadmin.ts
+++ src/pages/pesananadmin/pesananadmin.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DetailpesananPage } from '../detailpesanan/detailpesanan';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase,FirebaseListObservable } from 'angularfire2/database';
 /**
  * Generated class for the PesananadminPage page.
  *
@@ -14,9 +16,16 @@ import { DetailpesananPage } from '../detailpesanan/detailpesanan';
   templateUrl: 'pesananadmin.html',
 })
 export class PesananadminPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  DetailpesananPage:any;
+  transaksi:any;
+  detailtrans:any;
+  constructor(public navCtrl: NavController,private fireauth: AngularFireAuth,public db:AngularFireDatabase, public navParams: NavParams) {
+    this.DetailpesananPage=DetailpesananPage;
+    this.db.list('/transaksi/').subscribe(data => {
+          this.transaksi=data;
+          
+        });
+      }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PesananPage');
